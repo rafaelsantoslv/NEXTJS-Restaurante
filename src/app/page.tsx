35d74d05aaae48c426ade4c274542bbfcc8503bb
Rafael Santos <rafaeldsantos442@gmail.com>
2024-04-24 01:18:57 -0300

    Fix: corrigido soma dos valores totais, criado também a nova aba de pedidos.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,25 @@ import NavBar from "@/components/NavBar";
 import TableStatus from "@/components/TableMesas";
 
 const tables = [
-  { id: 1, status: "ocupada" },
-  { id: 2, status: "livre" },
-  { id: 3, status: "reservada" },
-  { id: 4, status: "ocupada" },
-  { id: 5, status: "livre" },
-  { id: 6, status: "reservada" },
-  { id: 7, status: "ocupada" },
-  { id: 8, status: "livre" },
-  { id: 9, status: "reservada" },
+  { id: 1, status: "ocupada", totalMesa: 0 },
+  { id: 2, status: "livre", totalMesa: 0 },
+  { id: 3, status: "reservada", totalMesa: 0 },
+  { id: 4, status: "ocupada", totalMesa: 0 },
+  { id: 5, status: "livre", totalMesa: 0 },
+  { id: 6, status: "reservada", totalMesa: 0 },
+  { id: 7, status: "ocupada", totalMesa: 0 },
+  { id: 8, status: "livre", totalMesa: 0 },
+  { id: 9, status: "reservada", totalMesa: 0 },
 
 ];
 
 const products = [
-  { id: 1, name: "Hamburguer", cod: 11, valor: '99,99', img: 'https://unsplash.com/photos/sc5sTPMrVfk/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzEwNTIxMjIwfA&force=true&w=640', quantidade: 1 },
-  { id: 2, name: "Batata-frita", cod: 12, valor: '99,99', img: 'https://unsplash.com/photos/lpsbMRRqMQw/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Nnx8ZnJpdGFzfHB0fDB8fHx8MTcxMDUyMTQ3MHww&force=true&w=640', quantidade: 1 },
-  { id: 3, name: "Frango frito", cod: 13, valor: '99,99', img: 'https://unsplash.com/photos/RWAToPPP9RY/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8ZnJhbmdvJTIwZnJpdG98cHR8MHx8fHwxNzEwNTIxNDk5fDA&force=true&w=640', quantidade: 1 },
-  { id: 4, name: "Camarão", cod: 14, valor: '99,99', img: 'https://unsplash.com/photos/4LoGKVmUJsI/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8Y2FtYXIlQzMlQTNvfHB0fDB8fHx8MTcxMDUyMTUzMnww&force=true&w=640', quantidade: 1 },
-  { id: 5, name: "Coca-cola", cod: 15, valor: '99,99', img: 'https://unsplash.com/photos/z8PEoNIlGlg/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzEwNTIxNTYzfA&force=true&w=640', quantidade: 1 },
+  { id: 1, name: "Hamburguer", cod: 11, valor: 99.99, img: 'https://unsplash.com/photos/sc5sTPMrVfk/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzEwNTIxMjIwfA&force=true&w=640', quantidade: 0 },
+  { id: 2, name: "Batata-frita", cod: 12, valor: 99.99, img: 'https://unsplash.com/photos/lpsbMRRqMQw/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Nnx8ZnJpdGFzfHB0fDB8fHx8MTcxMDUyMTQ3MHww&force=true&w=640', quantidade: 0 },
+  { id: 3, name: "Frango frito", cod: 13, valor: 99.99, img: 'https://unsplash.com/photos/RWAToPPP9RY/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8ZnJhbmdvJTIwZnJpdG98cHR8MHx8fHwxNzEwNTIxNDk5fDA&force=true&w=640', quantidade: 0 },
+  { id: 4, name: "Camarão", cod: 14, valor: 99.99, img: 'https://unsplash.com/photos/4LoGKVmUJsI/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8Y2FtYXIlQzMlQTNvfHB0fDB8fHx8MTcxMDUyMTUzMnww&force=true&w=640', quantidade: 0 },
+  { id: 5, name: "Coca-cola", cod: 15, valor: 99.99, img: 'https://unsplash.com/photos/z8PEoNIlGlg/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzEwNTIxNTYzfA&force=true&w=640', quantidade: 0 },
+
 ];
 
 
--- src/components/TableMesas/index.tsx
+++ src/components/TableMesas/index.tsx
@@ -1,24 +1,14 @@
 import React, { useState } from "react";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
-import {
-    Table as TableUI,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableCaption,
-    TableFooter,
-
-} from "@/components/ui/table"
+import { Table as TableUI, TableBody, TableCell, TableHead, TableHeader, TableRow, TableCaption, TableFooter } from "@/components/ui/table";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 
-
 interface Table {
     id: number;
     status: string;
+    totalMesa: number;
 }
 
 interface Product {
@@ -27,7 +17,7 @@ interface Product {
     cod: number;
     valor: string;
     img?: any;
-    quantidade?: any
+    quantidade?: any;
 }
 
 interface Props {
@@ -36,129 +26,26 @@ interface Props {
 }
 
 function TableStatus({ tables, products }: Props) {
-    const [searchTerm, setSearchTerm] = useState<string>('');
-    const [quantidade, setQuantidade] = useState(0)
-    const [itens, setItens] = useState(products)
-
-    const index = () => {
-        return (
-            <div>index</div>
-        )
-    }
-
-
-    const filteredProducts = itens.filter(product =>
-        product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-
-
-    const adicionarQuantidade = (id: Product) => {
-        const novosItens = itens.map(item => {
-            if (item.id === id) {
-                return { ...item, quantidade: item.quantidade + 1 }
-            }
-            return item
-        });
-
-        setItens(novosItens)
-    }
-
-    const removeQuantidade = (id: Product) => {
-        const novosItens = itens.map(item => {
-            if (item.id === id) {
-                return { ...item, quantidade: Math.max(0, item.quantidade - 1) }
-            }
-            return item
-        });
-        setItens(novosItens)
-    }
 
     return (
         <div className="grid grid-cols-3 gap-4 h-full w-full">
-            {tables.map(table => (
-                <Dialog key={table.id} >
-                    <DialogTrigger>
-                        <div
-                            key={table.id}
-                            className="p-4 border rounded shadow-md flex flex-col items-center hover:bg-slate-400 cursor-pointer"
-                        >
-                            <div className="text-xl font-semibold">Mesa {table.id}</div>
-                            <div
-                                className={`mt-2 px-4 py-1 rounded ${table.status === "ocupada"
-                                    ? "bg-red-500 text-white"
-                                    : table.status === "reservada"
-                                        ? "bg-yellow-500 text-gray-800"
-                                        : "bg-green-500 text-white"
-                                    }`}
-                            >
-                                {table.status}
-                            </div>
+            {tables.map(mesa => (
+                <div key={mesa.id}>
+                    <div className="p-4 border rounded shadow-md flex flex-col items-center">
+                        <div className="text-xl font-semibold">Mesa {mesa.id}</div>
+                        <div className={`mt-2 px-4 py-1 rounded ${mesa.status === "ocupada" ? "bg-red-500 text-white" : mesa.status === "reservada" ? "bg-yellow-500 text-gray-800" : "bg-green-500 text-white"}`}>
+                            {mesa.status}
                         </div>
-                    </DialogTrigger>
-                    <DialogContent>
-                        <DialogHeader>
-                            <DialogTitle>Novo pedido MESA {table.id}</DialogTitle>
-                            <DialogDescription>Selecione o Produto</DialogDescription>
-                        </DialogHeader>
-                        <form key={table.id} className="space-y-4">
-                            <div className="grid grid-cols-6 items-center text-center gap-3">
-                                <Label htmlFor='search'>Buscar Produto:</Label>
-                                <Input
-                                    id='search'
-                                    type='text'
-                                    value={searchTerm}
-                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
-                                    className="col-span-2"
-                                />
-                            </div>
-                            <div>
-                                <TableUI>
-                                    <TableCaption>PEDIDOS</TableCaption>
-                                    <HeaderTable />
-                                    <TableBody>
-                                        {filteredProducts.map(itens => (
-                                            <TableRow key={itens.id}>
-                                                <TableCell>
-                                                    <img src={itens.img}
-                                                        className="w-20 h-20" />
-                                                </TableCell>
-                                                <TableCell>{itens.name}</TableCell>
-                                                <TableCell>R$ {itens.valor}</TableCell>
-                                                <TableCell>
-                                                    <div className="flex space-x-1">
-                                                        <div><Button type="button" onClick={() => removeQuantidade(itens.id)}>-</Button></div>
-                                                        <div><Button>{itens.quantidade}</Button></div>
-                                                        <div><Button type="button" onClick={() => adicionarQuantidade(itens.id)}>+</Button></div>
-                                                    </div>
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                    <TableFooter>
-                                        <TableRow>
-                                            <TableCell>
-                                                Total
-                                            </TableCell>
-                                            <TableCell className="text-right">R$ 2500</TableCell>
-                                        </TableRow>
-                                    </TableFooter>
-                                </TableUI>
-                            </div>
-                            <DialogFooter>
-                                <DialogClose>
-                                    <Button type="button" variant='outline'>Cancelar</Button>
-                                    <Button type="button" className="ml-2">Confirmar</Button>
-                                </DialogClose>
-                            </DialogFooter>
-                        </form>
-                    </DialogContent>
-                </Dialog>
+                        <div className="space-x-3 space-y-3">
+                            <NovoPedido tables={tables} products={products} mesa={mesa} />
+                            <PedidosMesa tables={tables} products={products} mesa={mesa} />
+                        </div>
+                    </div>
+                </div>
             ))}
         </div>
     );
 }
-
 
 const HeaderTable = () => {
     return (
@@ -170,8 +57,203 @@ const HeaderTable = () => {
                 <TableHead>Quantidade</TableHead>
             </TableRow>
         </TableHeader>
+    );
+}
+
+const NovoPedido = ({ tables, products, mesa }: Props & { mesa: Table }) => {
+
+    const [itens, setItens] = useState<Product[]>(products);
+    const [searchTerm, setSearchTerm] = useState<string>('');
+
+    const filteredProducts = itens.filter(product =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const adicionarQuantidade = (productId: number, mesaId: number) => {
+        const novosItens = itens.map(item => {
+            if (item.id === productId) {
+                return { ...item, quantidade: (item.quantidade || 0) + 1 };
+            }
+            return item;
+        });
+
+        setItens(novosItens);
+    };
+
+    const removerQuantidade = (productId: number, mesaId: number) => {
+        const novosItens = itens.map(item => {
+            if (item.id === productId) {
+                return { ...item, quantidade: Math.max(0, (item.quantidade || 0) - 1) };
+            }
+            return item;
+        });
+
+        setItens(novosItens);
+    };
+
+    const atualizarTotalMesa = (itens: Product[]) => {
+        const totalMesa = itens.reduce((acc, cur) => {
+            return acc + (cur.quantidade || 0) * parseFloat(cur.valor);
+        }, 0);
+
+        return parseFloat(totalMesa.toFixed(2));
+    };
+
+    return (
+        <Dialog>
+            <DialogTrigger>
+                <Button variant={'outline'}>Novo Pedido</Button>
+            </DialogTrigger>
+            <DialogContent className="block">
+                <DialogHeader>
+                    <DialogTitle>Novo pedido MESA {mesa.id}</DialogTitle>
+                    <DialogDescription>Selecione o Produto</DialogDescription>
+                </DialogHeader>
+                <form className="space-y-4">
+                    <div className="grid grid-cols-1 items-center text-center gap-3">
+                        <Label htmlFor='search'>Buscar Produto:</Label>
+                        <Input
+                            id='search'
+                            type='text'
+                            value={searchTerm}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+                            className="col-span-2"
+                        />
+
+                        <TableUI>
+                            <TableCaption>PEDIDOS</TableCaption>
+                            <HeaderTable />
+                            <TableBody>
+                                {filteredProducts.map(item => (
+                                    <TableRow key={item.id}>
+                                        <TableCell><img src={item.img} className="w-20 h-20" /></TableCell>
+                                        <TableCell>{item.name}</TableCell>
+                                        <TableCell>R$ {item.valor}</TableCell>
+                                        <TableCell>
+                                            <div className="flex space-x-1">
+                                                <div><Button type="button" onClick={() => removerQuantidade(item.id, mesa.id)}>-</Button></div>
+                                                <div><Button>{item.quantidade}</Button></div>
+                                                <div><Button type="button" onClick={() => adicionarQuantidade(item.id, mesa.id)}>+</Button></div>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    <TableCell>Total R$ {atualizarTotalMesa(filteredProducts)}</TableCell>
+                                    <TableCell className="text-right"></TableCell>
+                                </TableRow>
+                            </TableFooter>
+                        </TableUI>
+                    </div>
+                    <DialogFooter>
+                        <DialogClose>
+                            <Button type="button" variant='outline'>Cancelar</Button>
+                            <Button type="button" className="ml-2">Confirmar</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
     )
 }
 
+const PedidosMesa = ({ tables, products, mesa }: Props & { mesa: Table }) => {
+
+    const [itens, setItens] = useState<Product[]>(products);
+    const [searchTerm, setSearchTerm] = useState<string>('');
+
+    const filteredProducts = itens.filter(product =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const adicionarQuantidade = (productId: number, mesaId: number) => {
+        const novosItens = itens.map(item => {
+            if (item.id === productId) {
+                return { ...item, quantidade: (item.quantidade || 0) + 1 };
+            }
+            return item;
+        });
+
+        setItens(novosItens);
+    };
+
+    const removerQuantidade = (productId: number, mesaId: number) => {
+        const novosItens = itens.map(item => {
+            if (item.id === productId) {
+                return { ...item, quantidade: Math.max(0, (item.quantidade || 0) - 1) };
+            }
+            return item;
+        });
+
+        setItens(novosItens);
+    };
+
+    const atualizarTotalMesa = (itens: Product[]) => {
+        const totalMesa = itens.reduce((acc, cur) => {
+            return acc + (cur.quantidade || 0) * parseFloat(cur.valor);
+        }, 0);
+
+        return parseFloat(totalMesa.toFixed(2));
+    };
+
+    return (
+        <Dialog>
+            <DialogTrigger>
+                <Button variant={'outline'}>Pedidos</Button>
+            </DialogTrigger>
+            <DialogContent className="block">
+                <DialogHeader>
+                    <DialogTitle>Pedidos MESA {mesa.id}</DialogTitle>
+                    <DialogDescription>Pedidos já feitos</DialogDescription>
+                </DialogHeader>
+                <form className="space-y-4">
+                    <div className="grid grid-cols-1 items-center text-center gap-3">
+                        <Label htmlFor='search'>Buscar Produto:</Label>
+                        <Input
+                            id='search'
+                            type='text'
+                            value={searchTerm}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+                            className="col-span-2"
+                        />
+
+                        <TableUI>
+                            <TableCaption>PEDIDOS</TableCaption>
+                            <HeaderTable />
+                            <TableBody>
+                                {filteredProducts.map(item => (
+                                    <TableRow key={item.id}>
+                                        <TableCell><img src={item.img} className="w-20 h-20" /></TableCell>
+                                        <TableCell>{item.name}</TableCell>
+                                        <TableCell>R$ {item.valor}</TableCell>
+                                        <TableCell>
+                                            <div className="flex space-x-1">
+                                                <div><Button disabled>{15 | item.quantidade}</Button></div>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    <TableCell>Total Mesa R$ {115 | atualizarTotalMesa(filteredProducts)}</TableCell>
+                                    <TableCell className="text-right"></TableCell>
+                                </TableRow>
+                            </TableFooter>
+                        </TableUI>
+                    </div>
+                    <DialogFooter>
+                        <DialogClose>
+                            <Button type="button" variant='outline'>Cancelar</Button>
+                            <Button type="button" className="ml-2">Fechamento</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
 
 export default TableStatus;
